c7015dca031c6d02f7b770713bdba8a37c782581
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-15 14:36:51 -0600

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { browser } from "$app/environment";
+
+  let firstNum = 0;
+  let secondNum = 0;
+  let thirdNum = 0;
+
+  if (browser) {
+    firstNum = prompt("Please enter any number of your choosing");
+    secondNum = prompt("enter another number");
+    thirdNum = prompt("last one!");
+  }
+</script>
+
+<main>
+  <h1>Assignment 5 - Loops and arrays</h1>
+  <p>{firstNum}, {secondNum}, {thirdNum}</p>
+</main>
